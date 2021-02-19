d5d1acec968cdb989129b12e34cba49b6cfb5b1a
Daan Dalman <daandalman@gmail.com>
2021-02-19 11:59:31 +0100

    adjustment for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   get 'cocktails/name'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :ingredients do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :ingredients, only: :destroy
 end
